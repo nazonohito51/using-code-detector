32d2cf701c230e03816b8b6c37cbbef7b702852e
kawashima <nazonohito51@gmail.com>
2018-03-06 18:38:34 +0900

    define throws Exceptions by phpdoc

--- src/Detector/StorageInterface.php
+++ src/Detector/StorageInterface.php
@@ -1,8 +1,21 @@
 <?php
 namespace CodeDetector\Detector;
 
+use CodeDetector\Exceptions\Storage\ConnectionException;
+
 interface StorageInterface
 {
+    /**
+     * @param string $key
+     * @return mixed
+     * @throws ConnectionException
+     */
     public function get($key);
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @throws ConnectionException
+     */
     public function set($key, $value);
 }
