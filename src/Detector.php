a96068ff15277ed8fccb34f6c48c2ddf2a28e4ae
kawashima <nazonohito51@gmail.com>
2018-03-08 16:50:23 +0900

    use storage in Detector

--- src/Detector.php
+++ src/Detector.php
@@ -7,8 +7,13 @@ use PHP_CodeCoverage;
 
 class Detector
 {
+    const STORAGE_KEY_PREFIX = 'CodeDetector';
+
+    private $ignoreFilePath;
     private $coverage;
     private $storage;
+
+    private $id;
 
     public function __construct(PHP_CodeCoverage $coverage, StorageInterface $storage)
     {
@@ -16,32 +21,73 @@ class Detector
         $this->storage = $storage;
     }
 
+    public function setIgnoreFilePathRegexp($regexp)
+    {
+        $this->ignoreFilePath = $regexp;
+    }
+
     public function start($id)
     {
+        $this->id = $id;
         $this->coverage->start($id);
     }
 
     public function stop()
     {
-        $data = $this->coverage->stop();
+        $this->coverage->stop();
+        $coverageData = $this->coverage->getData();
 
-//        // TODO: get past data
-//        $past_data = $this->storage->get(self::STORAGE_KEY);
-//
-//        // TODO: merge data
-//        $coverage = new PHP_CodeCoverage();
-//        $coverage->setData($past_data);
-//        // TODO: files in $data is checked by file_exists when merge, also setData...
-//        $this->coverage->merge($coverage);
+        foreach ($coverageData as $file => $lines) {
+            // TODO: convert file path
+            $key = $this->convertStorageKey($file);
+            // TODO: get past data
+            $pastData = $this->getData($key);
+            // TODO: merge data
+            foreach ($lines as $line => $execute) {
+                if ($execute == \PHP_CodeCoverage_Driver::LINE_EXECUTED) {
+                    if (empty($pastData[$line]) || !in_array($this->id, $pastData[$line])) {
+                        $pastData[$line][] = $this->id;
+                    }
+                }
+            }
+            // TODO: saveData
+            $this->saveData($key, $pastData);
 
-        $this->saveData($data);
+            // NOTICE: files in $data is checked by file_exists when merge, also setData...
+        }
+
+//        $this->saveData($coverageData);
     }
 
-    public function saveData($data)
+    private function convertStorageKey($path)
+    {
+        $hash = hash_file('md5', $path);
+        $path = preg_replace($this->ignoreFilePath, '', $path);
+
+        return self::STORAGE_KEY_PREFIX . ":{$path}:{$hash}";
+    }
+
+    private function getData($key)
+    {
+        try {
+            $data = $this->storage->get($key);
+            if (!is_null($data) && empty($data)) {
+                return unserialize($data);
+            }
+        } catch (ConnectionException $e) {
+            // TODO: notification
+        } catch (\Exception $e) {
+            // TODO: notification
+        }
+
+        return null;
+    }
+
+    private function saveData($key, $data)
     {
         // if saving data is failed, Detector will only notify, and not throw Exception.
         try {
-            $this->storage->set('CodeDetector', serialize($data));
+            $this->storage->set($key, serialize($data));
         } catch (ConnectionException $e) {
             // TODO: notification
         } catch (\Exception $e) {
