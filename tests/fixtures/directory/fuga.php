4897b400edd7768d5558f57715e9964c0803f155
kawashima <nazonohito51@gmail.com>
2018-03-16 21:22:59 +0900

    add test fixtures

---
+++ tests/fixtures/directory/fuga.php
@@ -0,0 +1,15 @@
+<?php
+
+if (true === false) {
+    echo 'This is test.';
+    echo 'This is test.';
+    echo 'This is test.';
+    echo 'This is test.';
+    echo 'This is test.';
+} else {
+    echo 'This is test.';
+    echo 'This is test.';
+    echo 'This is test.';
+    echo 'This is test.';
+    echo 'This is test.';
+}
---
+++ tests/fixtures/hoge.php
@@ -0,0 +1,5 @@
+<?php
+
+echo 'This is test.';
+echo 'This is test.';
+echo 'This is test.';
