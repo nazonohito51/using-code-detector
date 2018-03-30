d1119a00c5c9561fd4b8b91836ef54120a1f8778
kawashima <nazonohito51@gmail.com>
2018-03-30 19:34:22 +0900

    fix Detector

--- src/Detector.php
+++ src/Detector.php
@@ -1,6 +1,7 @@
 <?php
 namespace CodeDetector;
 
+use CodeDetector\Detector\CoverageData;
 use CodeDetector\Detector\Driver;
 use CodeDetector\Detector\StorageInterface;
 use CodeDetector\Exceptions\Storage\ConnectionException;
@@ -34,74 +35,73 @@ class Detector
 
     public function stop()
     {
-        $coverageData = $this->driver->stop();
+        $data = CoverageData::createFromXDebug($this->driver->stop(), $this->ignoreFilePath, $this->id);
+        $storageData = CoverageData::createFromStorage($this->storage);
+        $storageData->merge($data);
+        $storageData->save($this->storage);
 
-        foreach ($coverageData as $file => $lines) {
-            // TODO: convert file path
-            $key = $this->convertStorageKey($file);
-            // TODO: get past data
-            $pastData = $this->getData($key);
-            // TODO: merge data
-            foreach ($lines as $line => $execute) {
-                if ($execute == Driver::LINE_EXECUTED) {
-                    if (empty($pastData[$line]) || !in_array($this->id, $pastData[$line])) {
-                        $pastData[$line][] = $this->id;
-                    }
-                }
-            }
-            // TODO: saveData
-            $this->saveData($key, $pastData);
-
-            // NOTICE: files in $data is checked by file_exists when merge, also setData...
-        }
-
-//        $this->saveData($coverageData);
+//        foreach ($coverageData as $file => $lines) {
+//            // TODO: convert file path
+//            $key = $this->convertStorageKey($file);
+//            // TODO: get past data
+//            $pastData = $this->getData($key);
+//            // TODO: merge data
+//            foreach ($lines as $line => $execute) {
+//                if ($execute == Driver::LINE_EXECUTED) {
+//                    if (empty($pastData[$line]) || !in_array($this->id, $pastData[$line])) {
+//                        $pastData[$line][] = $this->id;
+//                    }
+//                }
+//            }
+//            // TODO: saveData
+//            $this->saveData($key, $pastData);
+//        }
     }
 
-    private function convertStorageKey($path)
-    {
-        $hash = hash_file('md5', $path);
-        $path = preg_replace($this->ignoreFilePath, '', $path);
-
-        return self::STORAGE_KEY_PREFIX . ":{$path}:{$hash}";
-    }
-
-    private function getData($key)
-    {
-        try {
-            $data = $this->storage->get($key);
-            if (!is_null($data) && !empty($data)) {
-                return unserialize($data);
-            }
-        } catch (ConnectionException $e) {
-            // TODO: notification
-        } catch (\Exception $e) {
-            // TODO: notification
-        }
-
-        return null;
-    }
-
-    private function saveData($key, $data)
-    {
-        // if saving data is failed, Detector will only notify, and not throw Exception.
-        try {
-            $this->storage->set($key, serialize($data));
-        } catch (ConnectionException $e) {
-            // TODO: notification
-        } catch (\Exception $e) {
-            // TODO: notification
-        }
-    }
-
-    public function getAllData()
-    {
-        try {
-            return $this->storage->getAll();
-        } catch (ConnectionException $e) {
-            // TODO: notification
-        } catch (\Exception $e) {
-            // TODO: notification
-        }
-    }
+//    private function convertStorageKey($path)
+//    {
+//        $hash = hash_file('md5', $path);
+//        $path = preg_replace($this->ignoreFilePath, '', $path);
+//
+//        return self::STORAGE_KEY_PREFIX . ":{$path}:{$hash}";
+//    }
+//
+//    private function getData($key)
+//    {
+//        try {
+//            $data = $this->storage->get($key);
+//            if (!is_null($data) && !empty($data)) {
+//                return unserialize($data);
+//            }
+//        } catch (ConnectionException $e) {
+//            // TODO: notification
+//        } catch (\Exception $e) {
+//            // TODO: notification
+//        }
+//
+//        return null;
+//    }
+//
+//    private function saveData($key, $data)
+//    {
+//        // if saving data is failed, Detector will only notify, and not throw Exception.
+//        try {
+//            $this->storage->set($key, serialize($data));
+//        } catch (ConnectionException $e) {
+//            // TODO: notification
+//        } catch (\Exception $e) {
+//            // TODO: notification
+//        }
+//    }
+//
+//    public function getAllData()
+//    {
+//        try {
+//            return $this->storage->getAll();
+//        } catch (ConnectionException $e) {
+//            // TODO: notification
+//        } catch (\Exception $e) {
+//            // TODO: notification
+//        }
+//    }
 }
