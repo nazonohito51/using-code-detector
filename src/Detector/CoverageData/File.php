6f4e57583030fb2fe011f222c2c6efbbb782177b
kawashima <nazonohito51@gmail.com>
2018-05-05 15:42:28 +0900

    add File class that manage coverage data about each files

---
+++ src/Detector/CoverageData/File.php
@@ -0,0 +1,121 @@
+<?php
+namespace CodeDetector\Detector\CoverageData;
+
+use CodeDetector\Detector\Storage\StorageInterface;
+use CodeDetector\Exceptions\InvalidFilePathException;
+use Webmozart\PathUtil\Path;
+
+class File
+{
+    const STORAGE_KEY_PREFIX = 'CodeDetector';
+
+    private $path;
+    private $coverage;
+    private $hash;
+
+    public function __construct($path, array $coverageLines = array(), $hash = null)
+    {
+        $this->path = $path;
+        $this->coverage = $coverageLines;
+        $this->hash = $hash;
+
+        if (is_null($this->hash)) {
+            if (!file_exists($path)) {
+                throw new InvalidFilePathException();
+            } else {
+                $this->hash = hash_file('md5', $this->path);
+            }
+        }
+    }
+
+    /**
+     * @param StorageInterface $storage
+     * @param string $rootDir
+     * @return File[]
+     */
+    public static function createFromStorage(StorageInterface $storage, $rootDir)
+    {
+        $files = array();
+
+        $data = $storage->getAll(self::STORAGE_KEY_PREFIX);
+        foreach ($data as $storageKey => $serializedCoverage) {
+            list($prefix, $path, $hash) = explode(':', $storageKey);
+            $coverage = unserialize($serializedCoverage);
+
+            $realPath = Path::join($rootDir, $path);
+            $files[$realPath] = new self($realPath, $coverage, $hash);
+        }
+
+        return $files;
+    }
+
+    public function isExist()
+    {
+        return file_exists($this->path) && $this->hash === hash_file('md5', $this->path);
+    }
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    public function getCoverage()
+    {
+        return $this->coverage;
+    }
+
+    public function append($id, $coverageLines)
+    {
+        if (!is_array($coverageLines)) {
+            $coverageLines = array($coverageLines);
+        }
+
+        foreach ($coverageLines as $line) {
+            $this->appendLine($id, $line);
+        }
+
+        ksort($this->coverage);
+    }
+
+    public function appendFile(File $file)
+    {
+        $coverage = $file->getCoverage();
+        foreach ($coverage as $line => $ids) {
+            if (!isset($this->coverage[$line])) {
+                $this->coverage[$line] = $ids;
+            } else {
+                $this->coverage[$line] = array_unique(array_merge($this->coverage[$line], $ids));
+            }
+        }
+
+        ksort($this->coverage);
+    }
+
+    private function appendLine($id, $line)
+    {
+        if (!isset($this->coverage[$line]) || !in_array($id, $this->coverage[$line])) {
+            $this->coverage[$line][] = $id;
+        }
+    }
+
+    public function save(StorageInterface $storage, $rootDir)
+    {
+        $key=$this->storageKey($rootDir);
+        $value=serialize($this->getCoverage());
+        $expect=serialize(array(
+            1 => array('fromXDebug'),
+            2 => array('fromXDebug'),
+            3 => array('fromStorage', 'fromXDebug'),
+            4 => array('fromStorage'),
+            5 => array('fromStorage'),
+        ));
+        $storage->set($this->storageKey($rootDir), serialize($this->getCoverage()));
+    }
+
+    private function storageKey($rootDir)
+    {
+        $relativePath = Path::makeRelative($this->path, $rootDir);
+
+        return self::STORAGE_KEY_PREFIX . ":{$relativePath}:{$this->hash}";
+    }
+}
---
+++ tests/Unit/Detector/CoverageData/FileTest.php
@@ -0,0 +1,142 @@
+<?php
+namespace CodeDetector\Detector\CoverageData;
+
+use CodeDetector\TestCase;
+use Mockery as m;
+
+class FileTest extends TestCase
+{
+    const ID1 = 'fromXDebug';
+    const ID2 = 'fromStorage';
+
+    public function testIsExist()
+    {
+        $existRealFile = new File($this->fixtures['file1']['path'], array(1 => self::ID1));
+        $notExistFile = new File($this->fixtures['file1']['path'], array(1 => self::ID1), md5('hoge'));
+
+        $this->assertTrue($existRealFile->isExist());
+        $this->assertFalse($notExistFile->isExist());
+    }
+
+    public function testRealPath()
+    {
+        $existRealFile = new File($this->fixtures['file1']['path'], array(1 => self::ID1));
+        $notExistFile = new File($this->fixtures['file1']['path'], array(1 => self::ID1), md5('hoge'));
+
+        $this->assertTrue($existRealFile->isExist());
+        $this->assertFalse($notExistFile->isExist());
+    }
+
+    public function testGetCoverage()
+    {
+        $file = new File($this->fixtures['file1']['path'], array(
+            1 => array(self::ID1),
+            2 => array(self::ID1),
+            3 => array(self::ID1),
+        ));
+
+        $this->assertEquals(array(
+            1 => array(self::ID1),
+            2 => array(self::ID1),
+            3 => array(self::ID1),
+        ), $file->getCoverage());
+
+        return $file;
+    }
+
+    /**
+     * @depends testGetCoverage
+     */
+    public function testAppend(File $file)
+    {
+        $file->append(self::ID1, array(3, 4, 5));
+
+        $this->assertEquals(array(
+            1 => array(self::ID1),
+            2 => array(self::ID1),
+            3 => array(self::ID1),
+            4 => array(self::ID1),
+            5 => array(self::ID1),
+        ), $file->getCoverage());
+
+        return $file;
+    }
+
+    /**
+     * @depends  testAppend
+     */
+    public function testAppendFile(File $file)
+    {
+        $appended = new File($this->fixtures['file1']['path'], array(
+            5 => array(self::ID2),
+            6 => array(self::ID2),
+            7 => array(self::ID2),
+        ));
+
+        $file->appendFile($appended);
+
+        $this->assertEquals(array(
+            1 => array(self::ID1),
+            2 => array(self::ID1),
+            3 => array(self::ID1),
+            4 => array(self::ID1),
+            5 => array(self::ID1, self::ID2),
+            6 => array(self::ID2),
+            7 => array(self::ID2),
+        ), $file->getCoverage());
+    }
+
+    public function testSave()
+    {
+        $storageMock = m::mock('CodeDetector\Detector\Storage\StorageInterface');
+        $storageMock->shouldReceive('set')->with($this->fixtures['file1']['storageKey'], serialize(array(
+            1 => array(self::ID1),
+            2 => array(self::ID1),
+            3 => array(self::ID1),
+        )))->once();
+
+        $file = new File($this->fixtures['file1']['path'], array(
+            1 => array(self::ID1),
+            2 => array(self::ID1),
+            3 => array(self::ID1),
+        ));
+
+        $file->save($storageMock, $this->reposRootDir());
+    }
+
+    public function testCreateFromStorage()
+    {
+        $storageMock = m::mock('CodeDetector\Detector\Storage\StorageInterface');
+        $storageMock->shouldReceive('getAll')->andReturn(array(
+            $this->fixtures['file1']['storageKey'] => serialize(array(
+                3 => array(self::ID1),
+                4 => array(self::ID1),
+                5 => array(self::ID1),
+            )),
+            $this->fixtures['file2']['storageKey'] => serialize(array(
+                10 => array(self::ID1),
+                11 => array(self::ID1),
+                12 => array(self::ID1),
+                13 => array(self::ID1),
+                14 => array(self::ID1),
+            ))
+        ));
+
+        $files = File::createFromStorage($storageMock, $this->reposRootDir());
+
+        $this->assertArrayHasKey($this->fixtures['file1']['path'], $files);
+        $this->assertEquals(array(
+            3 => array(self::ID1),
+            4 => array(self::ID1),
+            5 => array(self::ID1),
+        ), $files[$this->fixtures['file1']['path']]->getCoverage());
+        $this->assertArrayHasKey($this->fixtures['file2']['path'], $files);
+        $this->assertEquals(array(
+            10 => array(self::ID1),
+            11 => array(self::ID1),
+            12 => array(self::ID1),
+            13 => array(self::ID1),
+            14 => array(self::ID1),
+        ), $files[$this->fixtures['file2']['path']]->getCoverage());
+    }
+}
