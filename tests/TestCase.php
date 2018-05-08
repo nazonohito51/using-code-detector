d701b3a57b1a999893032fa2a09907223479fed0
kawashima <nazonohito51@gmail.com>
2018-05-08 18:13:49 +0900

    typo

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,8 @@
 <?php
 namespace CodeDetector;
 
+use CodeDetector\Detector\CoverageData\File;
+
 class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected $fixtures;
@@ -14,15 +16,15 @@ class TestCase extends \PHPUnit\Framework\TestCase
         $this->fixtures = array(
             'file1' => array(
                 'path' => $dir . '/hoge.php',
-                'storageKey' => Detector::STORAGE_KEY_PREFIX . ':tests/fixtures/hoge.php:7790190cbd3eba546205c88ce0682472'
+                'storageKey' => File::STORAGE_KEY_PREFIX . ':tests/fixtures/hoge.php:7790190cbd3eba546205c88ce0682472'
             ),
             'file2' => array(
                 'path' => $dir . '/directory/fuga.php',
-                'storageKey' => Detector::STORAGE_KEY_PREFIX . ':tests/fixtures/directory/fuga.php:65ac6a57264dcf93c28bbaf87660fce7'
+                'storageKey' => File::STORAGE_KEY_PREFIX . ':tests/fixtures/directory/fuga.php:65ac6a57264dcf93c28bbaf87660fce7'
             ),
             'file3' => array(
                 'path' => $dir . '/directory/piyo.php',
-                'storageKey' => Detector::STORAGE_KEY_PREFIX . ':tests/fixtures/directory/piyo.php:478de0143325e325388a60c6935981b8'
+                'storageKey' => File::STORAGE_KEY_PREFIX . ':tests/fixtures/directory/piyo.php:478de0143325e325388a60c6935981b8'
             )
         );
     }
